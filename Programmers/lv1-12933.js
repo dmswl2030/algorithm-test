c4b72ae248f1b4d90eef9deb17b289a72b9c9f0b
EUNJI <eunji9180@naver.com>
2023-02-22 18:51:38 +0900

    230222 algorithm : 프로그래머스 / lv1 / 정수 내림차순으로 배치하기

--- Programmers/lv1-12933.js
+++ Programmers/lv1-12933.js
@@ -2,10 +2,18 @@
 //함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
 
 function solution(n) {
-  const str = n.toString();
-  const arr = str.split("");
-  const sortedArr = arr.sort((a, b) => b - a);
-  const sortedStr = sortedArr.join("");
-
-  return parseInt(soredStr);
+  const str = n
+    .toString()
+    .split("")
+    .sort((a, b) => b - a)
+    .join("");
+  return parseInt(str);
 }
+console.log(solution(38901));
+
+/*
+[손코딩]
+1. 숫자 -> 문자열로 변경
+2. 하나씩 잘라서 정렬(내림차순)
+3. 다시 합치기
+*/
